9cc15fd1428a2fd5fff1b8ec4029e5da7e5a94f4
Pan Li <incarnation.p.lee@gmail.com>
2016-04-04 21:41:43 +0800

    Refined array queue resize API for expand and narrow.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- src/queue/impl/array_queue.c
+++ src/queue/impl/array_queue.c
@@ -1,21 +1,18 @@
 struct array_queue *
 array_queue_create(void)
 {
+    uint32 size;
     struct array_queue *queue;
 
+    size = DEFAULT_QUEUE_SPACE_SIZE;
     queue = memory_cache_allocate(sizeof(*queue));
-    if (!complain_no_memory_p(queue)) {
-        array_queue_sid_set(queue, 0x0u);
-    }
+    queue->space.base = memory_cache_allocate(sizeof(void *) * size);
 
-    queue->space.base = memory_cache_allocate(
-        sizeof(void *) * DEFAULT_QUEUE_SPACE_SIZE);
-    if (!complain_no_memory_p(queue->space.base)) {
-        queue->space.dim = DEFAULT_QUEUE_SPACE_SIZE;
-        queue->space.rest = DEFAULT_QUEUE_SPACE_SIZE;
-        queue->space.front = queue->space.base;
-        queue->space.rear = queue->space.base;
-    }
+    queue->space.dim = size;
+    queue->space.rest = size;
+
+    queue->space.front = queue->space.base;
+    queue->space.rear = queue->space.base;
 
     return queue;
 }
@@ -31,93 +28,125 @@ array_queue_destroy(struct array_queue **queue)
 }
 
 static inline void
-array_queue_space_expand_chunk_fixup(struct array_queue *queue, uint32 increment)
+array_queue_resize_expand(struct array_queue *queue, uint32 size,
+    void **addr)
 {
-    void **to;
-    void **from;
-    void **lmt;
+    uint32 part_size;
+    uint32 chunk_size;
 
-    assert(NULL != queue);
-    assert(0 != increment);
-    assert(queue->space.dim > increment);
+    assert(!complain_null_pointer_p(queue));
+    assert(!complain_null_pointer_p(addr));
+    assert(!complain_zero_size_p(size));
+    assert(size > array_queue_dim_m(queue));
 
-    lmt = queue->space.front;
-    to = queue->space.base + queue->space.dim;
-    from = queue->space.base + (queue->space.dim - increment);
+    if (queue->space.front < queue->space.rear) {
+        chunk_size = queue->space.rear - queue->space.front;
+        memcpy(addr, queue->space.front, sizeof(void *) * chunk_size);
 
-    while ((sint32)(lmt - from) < 0) {
-        *(--to) = *(--from);
+        queue->space.rear = addr + chunk_size;
+    } else if (queue->space.front >= queue->space.rear
+        && !array_queue_empty_p_internal(queue)) {
+        part_size = queue->space.front - queue->space.base;
+        chunk_size = array_queue_dim_m(queue) - part_size;
+        memcpy(addr, queue->space.front, sizeof(void *) * chunk_size);
+        part_size = queue->space.rear - queue->space.base;
+        memcpy(addr + chunk_size, queue->space.base, part_size);
+
+        queue->space.rear = addr + chunk_size + part_size;
+    } else {
+        queue->space.rear = addr;
     }
+
+    memory_cache_free(queue->space.base);
+    queue->space.base = addr;
+    queue->space.front = addr;
+    queue->space.rest += size - array_queue_dim_m(queue);
+    array_queue_dim_set_m(queue, size);
 }
 
 static inline void
-array_queue_space_expand_attr_update(struct array_queue *queue, uint32 increment,
-    void **new_addr)
+array_queue_resize_narrow(struct array_queue *queue, uint32 size,
+    void **addr)
 {
-    assert(NULL != queue);
-    assert(NULL != new_addr);
-    assert(0 != increment);
+    uint32 counted;
+    uint32 part_size;
+    uint32 chunk_size;
 
-    if (new_addr != queue->space.base) {
-        queue->space.front = new_addr + (queue->space.front - queue->space.base);
-        queue->space.rear = new_addr + (queue->space.rear - queue->space.base);
-    }
+    assert(!complain_null_pointer_p(queue));
+    assert(!complain_null_pointer_p(addr));
+    assert(!complain_zero_size_p(size));
+    assert(size < array_queue_dim_m(queue));
 
-    queue->space.base = new_addr;
-    queue->space.dim += increment;
-    queue->space.rest += increment;
-}
-
-static inline void
-array_queue_space_expand_internal(struct array_queue *queue, uint32 increment)
-{
-    uint32 new_size;
-    void **new_addr;
-
-    assert(NULL != queue);
-    assert(0 != increment);
-
-    new_size = array_queue_capacity_internal(queue) + increment;
-    new_addr = memory_cache_re_allocate(queue->space.base, sizeof(void *) * new_size);
-
-    if (!complain_no_memory_p(new_addr)) {
-        /*
-         * realloc may return a address different from queue->space.base 
-         */
-        array_queue_space_expand_attr_update(queue, increment, new_addr);
-        /*
-         * No need to do if (queue->space.front < queue->space.rear)
-         */
-        if ((queue->space.front == queue->space.rear && array_queue_full_p_internal(queue))
-            || queue->space.front > queue->space.rear) {
-            array_queue_space_expand_chunk_fixup(queue, increment);
+    if (queue->space.front < queue->space.rear) {
+        chunk_size = queue->space.rear - queue->space.front;
+        if (chunk_size >= size) {
+            pr_log_warn("Resize narrow will be trancated from tail.\n");
+            counted = size;
+        } else {
+            counted = chunk_size;
         }
+
+        memcpy(addr, queue->space.front, sizeof(void *) * counted);
+    } else if (queue->space.front >= queue->space.rear
+        && !array_queue_empty_p_internal(queue)) {
+        part_size = queue->space.front - queue->space.base;
+        chunk_size = array_queue_dim_m(queue) - part_size;
+
+        counted = MIN_U32(part_size, chunk_size);
+        memcpy(addr, queue->space.front, sizeof(void *) * counted);
+
+        if (chunk_size < size) {
+            part_size = size - counted;
+            chunk_size = queue->space.rear - queue->space.base;
+            chunk_size = MIN_U32(part_size, chunk_size);
+
+            memcpy(addr + counted, queue->space.base, sizeof(void *) * chunk_size);
+            counted += chunk_size;
+        }
+    } else {
+        counted = 0;
+        queue->space.rear = addr;
+    }
+
+    memory_cache_free(queue->space.base);
+    queue->space.base = addr;
+    queue->space.front = addr;
+    array_queue_dim_set_m(queue, size);
+    queue->space.rest = size - counted;
+    queue->space.rear = counted == size ? addr : addr + counted;
+}
+
+static inline void
+array_queue_resize_internal(struct array_queue *queue, uint32 size)
+{
+    void **addr;
+
+    assert(!complain_zero_size_p(size));
+    assert(!complain_null_pointer_p(queue));
+    assert(size != queue->space.dim);
+
+    addr = memory_cache_allocate(sizeof(void *) * size);
+
+    if (size > array_queue_dim_m(queue)) {
+        array_queue_resize_expand(queue, size, addr);
+    } else {
+        array_queue_resize_narrow(queue, size, addr);
     }
 }
 
 void
-array_queue_space_expand(struct array_queue *queue, uint32 extra)
+array_queue_resize(struct array_queue *queue, uint32 size)
 {
-    uint32 increment;
-
     if (!complain_null_pointer_p(queue)) {
-        if (!extra) {
-            increment = array_queue_capacity_internal(queue) + EXPAND_QUEUE_SPACE_MIN;
-            pr_log_info("Expanding size not specified, use default.\n");
-        } else {
-            increment = extra;
-        }
-
-        array_queue_space_expand_internal(queue, increment);
+        return;
+    } else if (size == queue->space.dim) {
+        return;
+    } else if (!complain_zero_size_p(size)) {
+        size = queue->space.dim * 2 + EXPAND_QUEUE_SPACE_MIN;
+        pr_log_info("Expanding size not specified, use default.\n");
     }
-}
 
-static inline uint32
-array_queue_capacity_internal(struct array_queue *queue)
-{
-    assert(NULL != queue);
-
-    return queue->space.dim;
+    array_queue_resize_internal(queue, size);
 }
 
 uint32
@@ -126,28 +155,20 @@ array_queue_capacity(struct array_queue *queue)
     if (complain_null_pointer_p(queue)) {
         return 0x0u;
     } else {
-        return array_queue_capacity_internal(queue);
+        return array_queue_dim_m(queue);
     }
-}
-
-static inline uint32
-array_queue_space_rest_internal(struct array_queue *queue)
-{
-    assert(NULL != queue);
-
-    return queue->space.rest;
 }
 
 /*
  * NULL array queue will _RETURN_ 0.
  */
 uint32
-array_queue_space_rest(struct array_queue *queue)
+array_queue_rest(struct array_queue *queue)
 {
     if (complain_null_pointer_p(queue)) {
-        return 0x0u;
+        return REST_INVALID;
     } else {
-        return array_queue_space_rest_internal(queue);
+        return array_queue_rest_m(queue);
     }
 }
 
@@ -156,7 +177,7 @@ array_queue_full_p_internal(struct array_queue *queue)
 {
     assert(NULL != queue);
 
-    return 0u == array_queue_space_rest_internal(queue) ? true : false;
+    return 0u == array_queue_rest_m(queue) ? true : false;
 }
 
 /*
@@ -180,8 +201,8 @@ array_queue_empty_p_internal(struct array_queue *queue)
 
     assert(NULL != queue);
 
-    capacity = array_queue_capacity_internal(queue);
-    rest = array_queue_space_rest_internal(queue);
+    capacity = array_queue_dim_m(queue);
+    rest = array_queue_rest_m(queue);
 
     return capacity == rest ? true : false;
 }
@@ -204,13 +225,13 @@ array_queue_enter(struct array_queue *queue, void *member)
 {
     if (!complain_null_pointer_p(queue) && !complain_null_pointer_p(member)) {
         if (array_queue_full_p_internal(queue)) {
-            array_queue_space_expand(queue, 0u);
+            array_queue_resize(queue, array_queue_dim_m(queue) * 2);
         }
 
         *queue->space.rear++ = member;
 
         if (queue->space.rear
-            == queue->space.base + array_queue_capacity_internal(queue)) {
+            == queue->space.base + array_queue_dim_m(queue)) {
             pr_log_info("Reach the limitation of array, will rotate.\n");
             queue->space.rear = queue->space.base;
         }
@@ -233,7 +254,7 @@ array_queue_leave(struct array_queue *queue)
         retval = *queue->space.front++;
 
         if (queue->space.front
-            == queue->space.base + array_queue_capacity_internal(queue)) {
+            == queue->space.base + array_queue_dim_m(queue)) {
             pr_log_info("Reach the limitation of array, will rotate.\n");
             queue->space.front = queue->space.base;
         }
@@ -250,7 +271,7 @@ array_queue_cleanup(struct array_queue *queue)
     uint32 capacity;
 
     if (!complain_null_pointer_p(queue)) {
-        capacity = array_queue_capacity_internal(queue);
+        capacity = array_queue_dim_m(queue);
         memset(queue->space.base, 0, sizeof(void *) * capacity);
 
         queue->space.rest = capacity;
@@ -265,20 +286,21 @@ array_queue_iterate(struct array_queue *queue, void (*handler)(void *))
     register void **iter;
     void **lmt;
 
-    if (!complain_null_pointer_p(queue) && !complain_null_pointer_p(handler)) {
-        if (array_queue_empty_p_internal(queue)) {
-            pr_log_info("Iterate on _EMPTY_ queue, nothing will be done.\n");
-        } else {
-            lmt = queue->space.base + array_queue_capacity_internal(queue);
-            iter = queue->space.front;
+    if (complain_null_pointer_p(queue) || complain_null_pointer_p(handler)) {
+        return;
+    } else if (array_queue_empty_p_internal(queue)) {
+        pr_log_info("Iterate on _EMPTY_ queue, nothing will be done.\n");
+        return;
+    } else {
+        lmt = queue->space.base + array_queue_dim_m(queue);
+        iter = queue->space.front;
 
-            do {
-                (*handler)(*iter++);
-                if (iter == lmt) {
-                    iter = queue->space.base;
-                }
-            } while (iter != queue->space.rear);
-        }
+        do {
+            (*handler)(*iter++);
+            if (iter == lmt) {
+                iter = queue->space.base;
+            }
+        } while (iter != queue->space.rear);
     }
 }
 
