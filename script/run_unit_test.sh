eb1cb696e85df0d39ea157aa29630636208c9d62
Pan Li <incarnation.p.lee@gmail.com>
2016-07-02 22:32:40 +0800

    DEBUG on travis ci of codecov
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -2,7 +2,12 @@
 
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
 exec ./output/out/ds.elf -u linked_list.*.*
+lcov --directory . --capture --output-file coverage.info
+mv coverage.info unit_test.info
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
 exec ./output/out/ds.elf -p=10000 linked_list.*.*
+lcov --directory . --capture --output-file coverage.info
+mv coverage.info performance_test.info
+lcov --add-tracefile unit_test.info --add-tracefile unit_test.info --output-file coverage.info
 
