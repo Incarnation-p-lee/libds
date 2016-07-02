3225eab5c7e2bb5e1a7d088687e7eee8a70fb9d3
Pan Li <incarnation.p.lee@gmail.com>
2016-07-03 07:48:32 +0800

    DEBUG on travis ci script
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -3,14 +3,18 @@
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
 exec ./output/out/ds.elf -u linked_list.*.*
 lcov --directory . --capture --output-file coverage.info
+set -x
 mv coverage.info /tmp/unit_test.info
 ls /tmp/unit_test.info
 file /tmp/unit_test.info
+set +x
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
 exec ./output/out/ds.elf -p=10000 linked_list.*.*
 lcov --directory . --capture --output-file coverage.info
+set -x
 mv coverage.info /tmp/performance_test.info
 ls /tmp/performance_test.info
 file /tmp/performance_test.info
+set +x
 
