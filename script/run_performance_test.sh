40fd116d6c414e8c297a56c79aedbcae7c05cd98
Pan Li <incarnation.p.lee@gmail.com>
2016-07-02 22:05:51 +0800

    Debug on bash of travis ci.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_performance_test.sh
+++ script/run_performance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./script/build.sh RELEASE X86_64 LIBC ELF
-./output/out/ds.elf -p=50000 linked_list.*.*
+bash ./script/build.sh RELEASE X86_64 LIBC ELF
+exec ./output/out/ds.elf -p=50000 linked_list.*.*
 
--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -u linked_list.*.*
+bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
+exec ./output/out/ds.elf -u linked_list.*.*
 
-./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -p=10000 linked_list.*.*
+bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
+exec ./output/out/ds.elf -p=10000 linked_list.*.*
 
