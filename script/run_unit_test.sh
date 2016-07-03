45e5a46713ea90361a93e79ea2446aa19fbf8643
Pan Li <incarnation.p.lee@gmail.com>
2016-07-03 16:49:49 +0800

    Debug if coverall donot allow big files.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -u *.*.*
+./output/out/ds.elf -u sort.*.*
 lcov --directory . --capture --output-file unit_test.info
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -p=100 *.*.*
+./output/out/ds.elf -p=100 sort.*.*
 lcov --directory . --capture --output-file performance_test.info
 
 lcov --add-tracefile unit_test.info --add-tracefile performance_test.info --output-file coverage.info
