7e324ed319cd86de107f3a88eaad0ee5b89e235e
Pan Li <incarnation.p.lee@gmail.com>
2016-07-03 17:34:05 +0800

    Debug coverall end.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -u sort.*.*
+./output/out/ds.elf -u *.*.*
 lcov --directory . --capture --output-file unit_test.info
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -p=100 sort.*.*
+./output/out/ds.elf -p=100 *.*.*
 lcov --directory . --capture --output-file performance_test.info
 
 lcov --add-tracefile unit_test.info --add-tracefile performance_test.info --output-file coverage.info
