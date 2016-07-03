ca05854613b52fa0b062361f70c64bd8af3a4074
Pan Li <incarnation.p.lee@gmail.com>
2016-07-03 08:55:14 +0800

    Merge unit test and performance test coverage report.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_performance_test.sh
+++ script/run_performance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF
-exec ./output/out/ds.elf -p=50000 linked_list.*.*
+exec ./output/out/ds.elf -p=50000 *.*.*
 
--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -u linked_list.*.*
+./output/out/ds.elf -u *.*.*
 lcov --directory . --capture --output-file coverage.info
 mv coverage.info unit_test.info
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
-./output/out/ds.elf -p=10000 linked_list.*.*
+./output/out/ds.elf -p=10000 *.*.*
 lcov --directory . --capture --output-file coverage.info
 mv coverage.info performance_test.info
 
 lcov --add-tracefile unit_test.info --add-tracefile performance_test.info --output-file coverage.info
+rm -v *.gcno
 
