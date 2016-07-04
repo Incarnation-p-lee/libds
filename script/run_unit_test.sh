7c46b2582e097d948302ace139f548e8bccbe007
Pan Li <Incarnation-p-lee@users.noreply.github.com>
2016-07-04 15:47:57 +0800

    Add branch coverage.

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+lcovrc=-rc lcov_branch_coverage=1 
+
 bash ./script/build.sh DEBUG X86_64 LIBC ELF COVERAGE
 ./output/out/ds.elf -u *.*.*
-lcov --directory . --capture --output-file unit_test.info
+lcov --directory . --capture $lcover --output-file unit_test.info
 
 bash ./script/build.sh RELEASE X86_64 LIBC ELF COVERAGE
 ./output/out/ds.elf -p=100 *.*.*
-lcov --directory . --capture --output-file performance_test.info
+lcov --directory . --capture $lcover --output-file performance_test.info
 
 lcov --add-tracefile unit_test.info --add-tracefile performance_test.info --output-file coverage.info
 cp -v coverage.info /tmp/coverage.info
 rm -v *.gcno
-
