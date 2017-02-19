670ff15c641ac475318fbd89968aa1e8e41fcd46
Pan Li <incarnation.p.lee@gmail.com>
2017-02-19 17:31:53 +0800

    DEBUG on travis CI.
    Signed-off-by: Pan Li <incarnation.p.lee@gmail.com>

--- script/run_unit_test.sh
+++ script/run_unit_test.sh
@@ -11,6 +11,13 @@ make clean && make RELEASE=1 COVERAGE=1
 ./bin/ds.elf -p=10 *.*.*
 lcov --directory . --capture $lcover --output-file performance_test.info
 
+## copy *.gcno file to current ##
+gcno_list=`find src/ | grep "gcno$"`
+for gcno in ${gcno_list[@]}
+do
+    cp $gcno .
+done
+
 lcov --add-tracefile unit_test.info --add-tracefile performance_test.info --output-file coverage.info
 cp -v coverage.info /tmp/coverage.info
 
